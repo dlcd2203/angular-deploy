85eba14d712c592a5c79c07416c4de77111d41e6
De Do <de.do@gooddata.com>
2020-05-25 10:25:36 +0700

    FEATURE: setting mapboxToken

--- src/utils/fixturesGeoChart.js
+++ src/utils/fixturesGeoChart.js
@@ -1,6 +1,7 @@
 import { projectId } from "./fixtures";
+import { environment } from '@environments/environment';
 
-export const MAPBOX_TOKEN = process.env.EXAMPLE_MAPBOX_ACCESS_TOKEN;
+export const MAPBOX_TOKEN = environment.EXAMPLE_MAPBOX_ACCESS_TOKEN;
 // export const MAPBOX_TOKEN = "pk.eyJ1IjoidGh1b25nZ29vZGRhdGEiLCJhIjoiY2s5aThtb3NrMTF1MjNsdGIxOTdqcXlvcyJ9.n0dhhtSmKxTZZYLBo8sfbw";
 export const cityCoordinatesUri = `/gdc/md/${projectId}/obj/9459`; // attr dedicated to geo chart with textual and geo DF
 export const populationUri = `/gdc/md/${projectId}/obj/9466`;
